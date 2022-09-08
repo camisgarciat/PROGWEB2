df0a2f7dc0388281fa5dce7f7d2e1240f46335d1
camisgarciat <104939161+camisgarciat@users.noreply.github.com>
2022-09-08 14:30:43 -0300

    Update damas.js

--- damas.js
+++ damas.js
@@ -1,57 +1,59 @@
-const tamanhoCelula = 40;
-let pecaId = 0;
-document.body.append(criaTabuleiro());
+const tamanhoCelula = 40
+let pecaId = 0
+let imgid 
+document.body.append(criaTabuleiro())
 
 function criaTabuleiro() {
-    const tamanho = 8;
-    let tabela = document.createElement('table');
+    const tamanho = 8
+    const tabela = document.createElement('table')
 
-    tabela.style.borderStyle = 'solid';
-    tabela.style.borderSpacing = 0;
-    tabela.style.margin = 'auto';
+    tabela.style.borderStyle = 'solid'
+    tabela.style.borderSpacing = 0
+    tabela.style.margin = 'auto'
 
     for (let i = 0; i < tamanho; i++) {
-        let linha = document.createElement('tr');
+        const linha = document.createElement('tr')
         tabela.append(linha);
         for (let j = 0; j < tamanho; j++) {
-            let celula = document.createElement('td');
-            linha.append(celula);
-
-            celula.style.width = `${tamanhoCelula}px`;
-            celula.style.height = `${tamanhoCelula}px`;
+            const celula = document.createElement('td')
+            celula.dataset.lin = i
+            celula.dataset.col = j
+            linha.append(celula)
+            celula.style.width = `${tamanhoCelula}px`
+            celula.style.height = `${tamanhoCelula}px`
             if (i % 2 == j % 2) {
-                celula.style.backgroundColor = 'black';
+                celula.addEventListener('dragover', permDrop)
+                celula.style.backgroundColor = 'black'
                 if (i * 8 + j <= 24) {
-                    celula.append(criaPeca('black'));
+                    const peca = criaPeca('black')
+                    peca.setAttribute('draggable','false')
+                    celula.append(peca)
+                    celula.removeEventListener('dragover', permDrop)
                 } else if (i * 8 + j >= 40) {
-                    celula.append(criaPeca('red'));
+                    celula.append(criaPeca('red'))
+                    celula.removeEventListener('dragover', permDrop)
                 }
             } else {
-                celula.style.backgroundColor = 'white';
+                celula.style.backgroundColor = 'white'
             }
         }
     };
     return tabela;
 }
 
-function criaPeca(black) {
-    let imagem =
-      document.createElement('img');
-    imagem.setAttribute('src', `img/${cor}.png`);
-    imagem.setAttribute('width', `${tamanhoCelula-4}px`);
-    imagem.setAttribute('height', `${tamanhoCelula-4}px`);
+function criaPeca(cor) {
+    const imagem = document.createElement('img')
+    imagem.classList.add('peca') 
+    imagem.id = `p${pecaId++}`
+    imagem.setAttribute('src', `img/${cor}.png`)
+    imagem.setAttribute('width', `${tamanhoCelula-4}px`)
+    imagem.setAttribute('height', `${tamanhoCelula-4}px`)
+    imagem.addEventListener('drag', drag)
     return imagem
 }
-  
-function criaPeca(cor) {
-    let imagem = document.createElement('img');
-    imagem.setAttribute('src', `${cor}.png`);
-    imagem.setAttribute('width', `${tamanhoCelula-4}px`);
-    imagem.setAttribute('height', `${tamanhoCelula-4}px`);
-    return imagem;
-}
+
 function permDrop(evento){
-    evento.preventDefault() 
+    evento.preventDefault() // Não vai fazer o normal
     const imagem = document.querySelector(`#${imgid}`)
     const col_ori = imagem.parentElement.dataset.col 
     const lin_ori = imagem.parentElement.dataset.lin
